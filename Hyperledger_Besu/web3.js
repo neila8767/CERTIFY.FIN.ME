b2af6e141fc06f622e9bbe7b1434509d79fedb97
neila8767 <neilabehidj7@gmail.com>
2025-05-27 13:37:06 +0100

    Remove Hyperledger_Besu obsolete files

--- Hyperledger_Besu/web3.js
+++
@@ -1,7 +0,0 @@
-import Web3 from "web3";
-
-// Connecte à la blockchain locale (Besu sur port 8545)
-const provider = new Web3.providers.HttpProvider("http://localhost:8545");
-const web3 = new Web3(provider);
-
-export default web3;
